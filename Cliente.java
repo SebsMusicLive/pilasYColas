127a7b63c07c9f6fef0c4b376d530a8d0d6882c8
SebsMusicLive <lopezortegajohansebastian@gmail.com>
2023-05-11 17:07:41 -0500

    Se agregan los archivos

---
+++ Cliente.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pilasYColas;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Cliente {
+    public static void main(String[] args) {
+        
+    }
+}
